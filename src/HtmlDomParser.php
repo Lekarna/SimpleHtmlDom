aec077a77af22a05ab4aa55886fc05dd28a37d2e
zener05 <zeneration@gmail.com>
2019-10-01 09:26:50 +0200

    Added checking return type

--- src/HtmlDomParser.php
+++ src/HtmlDomParser.php
@@ -6,15 +6,17 @@ namespace Lekarna\SimpleHtmlDom;
 class HtmlDomParser
 {
 
-	static public function file_get_html(): simple_html_dom
+	static public function file_get_html(): ?simple_html_dom
 	{
-		return call_user_func_array('\Lekarna\SimpleHtmlDom\file_get_html' , func_get_args());
+		$htmlDom = call_user_func_array('\Lekarna\SimpleHtmlDom\file_get_html' , func_get_args());
+		return $htmlDom instanceof simple_html_dom ? $htmlDom : NULL;
 	}
 
 
-	static public function str_get_html(): simple_html_dom
+	static public function str_get_html(): ?simple_html_dom
 	{
-		return call_user_func_array('\Lekarna\SimpleHtmlDom\str_get_html' , func_get_args());
+		$htmlDom = call_user_func_array('\Lekarna\SimpleHtmlDom\str_get_html' , func_get_args());
+		return $htmlDom instanceof simple_html_dom ? $htmlDom : NULL;
 	}
 
 }
